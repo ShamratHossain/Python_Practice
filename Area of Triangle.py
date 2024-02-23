7312afce6c61d9d1329e99ecef6a88670aba8a50
Md. Shamrat Hossain <hossain.shamratopu@gmail.com>
2024-02-23 06:34:46 -0500

    added new feature that calculates rectangle area

--- Area of Triangle.py
+++ Area of Triangle.py
@@ -1,16 +1,24 @@
 """
 This is a function called calculate_area that takes base and height as an input and returns and area of a triangle.
 Equation of an area of a triangle is, area = (1/2)*base*height
+It can be either "triangle" or "rectangle".
+Based on shape type it will calculate area.
+Equation of rectangle's area is, rectangle area=length*width
 """
 
-def calculate_area(base,height):
-    area = (1/2) * base * height
-    return area
+def calculate_area(base, height, shape = 'triangle'):
+    if shape== 'triangle':
+        area = (1/2) * base * height
+        return area
+    elif shape == 'rectangle':
+        area = base * height
+        return area
 
 
-new_base = int(input("Type the base value of your triangle: "))
-new_height = int(input("Type the height value of your triangle: "))
+shape = input("What is the shape you want to calculate('triangle' or 'rectangle'): ")
+new_base = int(input("Type the base/width value of your shape: "))
+new_height = int(input("Type the height/length value of your shape: "))
 
-triangel_area = calculate_area(new_base,new_height)
+shape_area = calculate_area(new_base, new_height, shape)
 
-print("The area of your triangel is ", triangel_area)
+print("The area of your shape is ", shape_area)
